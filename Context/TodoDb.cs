9b1887ec90026ef95e04fd695a2abaff8ffd5c1e
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-30 09:50:41 -0300

    Adicionar arquivos de projeto.

---
+++ Context/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimaAPI.Entidade;
+
+namespace MinimaAPI.Context
+{
+    class TodoDb: DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> option) : base(option) { }
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
